2f4673c81de0748b8fcc12e1c6ec375d7c203954
Onur Şentürk <onur.senturk@ideasoft.com.tr>
2020-04-08 11:44:44 +0300

    Private Access

--- lib/Ship.php
+++ lib/Ship.php
@@ -1,10 +1,10 @@
 <?php
 class Ship
 {
-    public $name;
-    public $weaponPower = 0;
-    public $jediFactor = 0;
-    public $strength = 0;
+    private $name;
+    private $weaponPower = 0;
+    private $jediFactor = 0;
+    private $strength = 0;
     public function sayHello()
     {
         echo 'Hello!';
@@ -39,4 +39,57 @@ class Ship
 
         return $givenShip->strength > $this->strength;
     }
+    public function setStrength($number)
+    {
+        if (!is_numeric($number)) {
+            throw new \Exception('Strength must be a number, duh!');
+        }
+        $this->strength = $number;
+    }
+    public function getStrength()
+    {
+        return $this->strength;
+    }
+
+    /**
+     * @return int
+     */
+    public function getWeaponPower()
+    {
+        return $this->weaponPower;
+    }
+
+    /**
+     * @param int $weaponPower
+     */
+    public function setWeaponPower($weaponPower)
+    {
+        $this->weaponPower = $weaponPower;
+    }
+
+    /**
+     * @return int
+     */
+    public function getJediFactor()
+    {
+        return $this->jediFactor;
+    }
+
+    /**
+     * @param int $jediFactor
+     */
+    public function setJediFactor($jediFactor)
+    {
+        $this->jediFactor = $jediFactor;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+
 }
