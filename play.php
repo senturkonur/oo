ecfd470401be7d02de307a756556a89bba1c2b19
Onur Şentürk <onur.senturk@ideasoft.com.tr>
2020-04-08 10:49:22 +0300

    class methods

--- play.php
+++ play.php
@@ -2,9 +2,27 @@
 class Ship
 {
     public $name;
+    public $weaponPower = 0;
+    public $jediFactor = 0;
+    public $strength = 0;
+    public function sayHello()
+    {
+        echo 'Hello!';
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
 }
 
 $myShip = new Ship();
 $myShip->name = 'TIE Fighter';
 
 echo 'Ship Name: '.$myShip->name;
+echo '<hr/>';
+$myShip->sayHello();
+echo '<hr/>';
+echo 'Ship Name: '.$myShip->getName();
+$myShip->weaponPower = 10;
+var_dump($myShip->weaponPower);
