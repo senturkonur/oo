eb0290d6b3a122bd2efa7c364561a9e811f8dc02
Onur Şentürk <onur.senturk@ideasoft.com.tr>
2020-04-08 17:34:59 +0300

    Optional type-hinting & Semantic Methods

--- lib/BattleResult.php
+++ lib/BattleResult.php
@@ -6,7 +6,7 @@ class BattleResult
     private $usedJediPowers;
     private $winningShip;
     private $losingShip;
-    public function __construct($usedJediPowers, Ship $winningShip, Ship $losingShip)
+    public function __construct($usedJediPowers, Ship $winningShip = null, Ship $losingShip = null)
     {
         $this->usedJediPowers = $usedJediPowers;
         $this->winningShip = $winningShip;
@@ -22,7 +22,7 @@ class BattleResult
     }
 
     /**
-     * @return Ship
+     * @return Ship |null
      */
     public function getWinningShip()
     {
@@ -30,11 +30,15 @@ class BattleResult
     }
 
     /**
-     * @return Ship
+     * @return Ship | null
      */
     public function getLosingShip()
     {
         return $this->losingShip;
+    }
+    public function isThereAWinner()
+    {
+        return $this->getWinningShip() !== null;
     }
 
 
